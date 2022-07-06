56fced146921f3455235b3fb3fcbf7f7ecaa8e88
Obed Paz <lester.paz@cit.hn>
2022-07-06 10:19:16 -0600

    Update app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CategoriesController } from './controllers/categories/categories.controller';
-import { ProductsController } from './controllers/products/products.controller';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController, ProductsController, CategoriesController],
+  imports: [ProductsModule],
+  controllers: [AppController, CategoriesController],
   providers: [AppService],
 })
 export class AppModule {}
