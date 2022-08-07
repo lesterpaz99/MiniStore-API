aa90dd629a6fccbad5a73678e0c28357f8a0d41c
Obed Paz <lester.paz@cit.hn>
2022-08-07 15:53:49 -0600

    Enable cors and add config for docs

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -12,6 +13,22 @@ async function bootstrap() {
       transform: true,
     })
   );
-  await app.listen(3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('My store API')
+    .setDescription('This is a sample API for my store build with NestJS')
+    .setVersion('1.0')
+    // .addTag('nestjs')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  app.enableCors({
+    origin: ['http://localhost:4200', 'http://localhost:3000'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+  });
+
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
