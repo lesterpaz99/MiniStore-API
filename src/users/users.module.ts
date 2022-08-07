4e6826f6a3672c16582159fe764e9d40f5b0a695
Obed Paz <lester.paz@cit.hn>
2022-08-07 15:51:15 -0600

    Update users module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './services/users.service';
 import { UsersController } from './controllers/users.controller';
+import { ProductsModule } from 'src/products/products.module';
 
 @Module({
+  imports: [ProductsModule],
   controllers: [UsersController],
   providers: [UsersService],
 })
