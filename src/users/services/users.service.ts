6b167f08f7219898b7cef7a47569c5982145ed15
Obed Paz <lester.paz@cit.hn>
2022-08-07 15:50:42 -0600

    Add getOrderByUser to user service

--- src/users/services/users.service.ts
+++ src/users/services/users.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto, UpdateUserDto } from '../dto/user.dto';
+import { ProductsService } from 'src/products/services/products.service';
+import { Order } from '../entities/order.entity';
 
 @Injectable()
 export class UsersService {
+  constructor(private productService: ProductsService) {}
+
   create(createUserDto: CreateUserDto) {
     return 'This action adds a new user';
   }
@@ -13,6 +17,16 @@ export class UsersService {
 
   findOne(id: number) {
     return `This action returns a #${id} user`;
+  }
+
+  getOrdersByUser(id: number) {
+    const user = this.findOne(id);
+    const products = this.productService.findAll();
+    return {
+      date: new Date(),
+      user,
+      products,
+    };
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
