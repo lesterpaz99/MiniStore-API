4c21005df09f8d3d8a33f4500f0c2d34023d9b63
Obed Paz <lester.paz@cit.hn>
2022-08-07 15:53:23 -0600

    Update the app service

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { ConfigType } from '@nestjs/config';
+import config from './config';
 
 @Injectable()
 export class AppService {
+  constructor(
+    @Inject(config.KEY) private configService: ConfigType<typeof config>
+  ) {}
   getHello(): string {
-    return 'Hello World!';
+    // console.log('Tasks => ', this.tasks);
+    // return 'Hello World! - ' + this.apiKey;
+    const apiKey = this.configService.apiKey;
+    const dbName = this.configService.database.name;
+    return apiKey + ' - ' + dbName;
   }
+
+  // async getTasks() {
+  //   return await this.httpService.get('');
+  // }
 }
